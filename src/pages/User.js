32991ecefe0a577ffc931113dc6352b0262d8198
santiago villalobos <55002307+SanjeVicor@users.noreply.github.com>
2021-04-04 20:01:13 -0500

    users backend integration set up set up

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,27 +1,73 @@
 import { Helmet } from 'react-helmet';
-import { Box, Container } from '@material-ui/core';
+import {
+  Box,
+  Container,
+  Alert,
+  AlertTitle,
+} from '@material-ui/core';
 import AccountListResults from 'src/components/account-scrumming/AccountListResults';
 import AccountListToolbar from 'src/components/account-scrumming/AccountListToolbar';
-import users from 'src/__mocks__/users';
+
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// import users from 'src/__mocks__/users';
 
 const User = () => {
-  const [localUsers, setUsers] = useState([]);
+  const [localUsers, setLocalUsers] = useState([]);
+  const [originalUsers, setOriginalUsers] = useState([]);
   const [selectedUsers, setSelectedUsers] = useState([]);
 
+  const [dialog, setDialog] = useState(null);
+
   useEffect(() => {
-    setUsers(users);
+    // setUsers(users);
+    console.log('Fetching Users');
+    const fetchData = async () => {
+      let users = [];
+      const config = {
+        method: 'get',
+        url: `${process.env.REACT_APP_API_URL}/user`,
+        headers: {
+          Authorization: '{{TOKEN}}'
+        }
+      };
+      await axios(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+          users = response.data;
+          setDialog(null);
+        })
+        .catch((error) => {
+          console.log(error);
+          setDialog(
+            <Alert severity="error">
+              <AlertTitle>
+                info
+              </AlertTitle>
+              message:
+              <br />
+              <strong>
+                Error fetching data, please try later or contact support.
+              </strong>
+            </Alert>
+          );
+        });
+      setLocalUsers(users);
+      setOriginalUsers(users);
+    };
+    fetchData();
   }, []);
 
   const handleSearchData = (data) => {
     if (data !== '') {
-      const aux = users.filter((user) => {
+      const aux = originalUsers.filter((user) => {
         const name = `${user.firstName} ${user.lastName}`;
         return name.toLowerCase().match(data);
       });
-      setUsers(aux);
+      setLocalUsers(aux);
     } else {
-      setUsers(users);
+      setLocalUsers(originalUsers);
     }
   };
 
@@ -43,6 +89,7 @@ const User = () => {
       >
         <Container maxWidth={false}>
           <AccountListToolbar handleSearchData={handleSearchData} handleSelectedUsers={selectedUsers} />
+          {dialog}
           <Box sx={{ pt: 3 }}>
             <AccountListResults users={localUsers} handleSelectedUsers={handleSelectedUsers} />
           </Box>
